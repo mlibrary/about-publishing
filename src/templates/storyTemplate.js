698dfee0ac87e70e056eb3d571cee69c3977e53f
Chris Keller <ckeller.tomgeekery@gmail.com>
2019-09-17 09:03:43 -0400

    Story template styling

--- src/templates/storyTemplate.js
+++ src/templates/storyTemplate.js
@@ -10,15 +10,41 @@ export default function Template({ data }) {
   return (
     <Layout>
       <SEO title={frontmatter.title} />
-      <div>{frontmatter.categories.join("|")}</div>
-      <h1>{frontmatter.title}</h1>
-      <img src={frontmatter.image.file} alt={frontmatter.image.alt} />
-      <div dangerouslySetInnerHTML={{ __html: html }} />
-      <div>By {frontmatter.author}</div>
-      <time datetime={frontmatter.plainDate}>{frontmatter.date}</time>
+
+      <div className="flex">
+        <div className="w-2/12"></div>
+
+        <div className="w-10/12 max-w-2xl">
+          <p className="text-sm uppercase text-dusk-blue mb-0">
+            {frontmatter.categories.join("|")}
+          </p>
+          <h1 className="font-serif text-375 mb-16 leading-none">
+            {frontmatter.title}
+          </h1>
+        </div>
+      </div>
+
+      <div className="flex items-start">
+        <div className="w-2/12 sticky top-2">
+          <p className="font-serif uppercase text-2xl">Share this</p>
+          <div className="flex justify-between max-w-8125">
+            <img src="/assets/facebook.svg" alt="facebook"/>
+            <img src="/assets/linkedin.svg" alt="facebook"/>
+            <img src="/assets/twitter.svg" alt="facebook"/>
+          </div>
+        </div>
+
+        <div className="w-10/12 max-w-2xl">
+          <img src={frontmatter.image.file} alt={frontmatter.image.alt} className="mb-16" />
+          <div dangerouslySetInnerHTML={{ __html: html }} />
+          <div>By {frontmatter.author}</div>
+          <time datetime={frontmatter.plainDate}>{frontmatter.date}</time>
+        </div>
+      </div>
     </Layout>
   )
 }
+
 export const pageQuery = graphql`
   query($path: String!) {
     markdownRemark(frontmatter: { path: { eq: $path } }) {
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,12 +3,17 @@ module.exports = {
     extend: {
       fontSize: {
         "35": "3.5rem",
+        "375": "3.75rem",
         "525": "5.25rem",
         "6": "6rem",
       },
       inset: {
         "-1": "-1rem",
+        "2": "2rem",
         "9": "9rem",
+      },
+      maxWidth: {
+        "8125": "8.125rem",
       },
       spacing: {
         "1675": "16.75rem",
