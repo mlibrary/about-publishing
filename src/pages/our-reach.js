93e96f3053c4de29768a48796bd5e1a69af0a620
Chris Keller <ckeller.tomgeekery@gmail.com>
2019-09-20 15:52:05 -0400

    Added readership map title to iframe

--- src/pages/our-reach.js
+++ src/pages/our-reach.js
@@ -11,7 +11,7 @@ class OurReachPage extends Component {
         <SEO title="Our Reach" />
         <PageHeading heading="Our Reach"></PageHeading>
         <p className="text-lg mb-6">Michigan Publishing serves faculty, staff, and students across the campus.</p>
-        <iframe width="100%" height="800" src="https://www.publishing.umich.edu/readership-map/"></iframe>
+        <iframe title="Readership map" width="100%" height="800" src="https://www.publishing.umich.edu/readership-map/"></iframe>
       </Layout>
     )
   }
