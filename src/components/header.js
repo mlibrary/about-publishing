b46544829a0860037a4629317781bdd45c9bd296
Chris Keller <ckeller.tomgeekery@gmail.com>
2019-09-30 09:38:46 -0400

    Our reach is now a regular page

--- src/components/header.js
+++ src/components/header.js
@@ -18,7 +18,7 @@ class Header extends Component {
     return (
       <header className="border-b border-almost-black-21">
         <div
-          className={`fixed inset-0 bg-almost-black-30 z-10 ${
+          className={`fixed inset-0 bg-almost-black-30 z-8 ${
             this.state.active ? "block" : "hidden"
           }`}
           onClick={this.hamburgerClick}
@@ -34,7 +34,7 @@ class Header extends Component {
             </AniLink>
           </h1>
           <div
-            className={`navigation lg:static lg:w-7/12 fixed left-0 top-0 bottom-0 bg-white z-10 w-320 lg:w-auto lg:translate-x-0 ${
+            className={`navigation lg:static lg:w-7/12 fixed left-0 top-0 bottom-0 bg-white z-8 w-320 lg:w-auto lg:translate-x-0 ${
               this.state.active
                 ? "transition-transform translate-x-0"
                 : "transition-transform -translate-x-320"
---
+++ src/content/pages/our-reach.md
@@ -0,0 +1,8 @@
+---
+title: Our Reach
+path: /our-reach
+type: page
+---
+Michigan Publishing serves faculty, staff, and students across the campus.
+
+<iframe title="Readership map" width="100%" height="800" src="https://www.publishing.umich.edu/readership-map/"></iframe>
