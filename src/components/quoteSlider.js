ffe5d61a2d860f7843301a52f77531089a020ec5
Chris Keller <kellerch@umich.edu>
2020-08-12 13:46:38 -0400

    Fix build errors

--- src/components/quoteSlider.js
+++ src/components/quoteSlider.js
@@ -4,18 +4,23 @@ import Quote from "../components/quote"
 import { Slide } from "../components/slide"
 
 const QuoteSlider = ({ content, books, slides }) => {
+  let next
+  let prev
   const [slidePosition, setSlide] = useState(0)
-  const slideMax = (slides.length - 1) * -41.666667
 
-  const next = () => {
-    if (slidePosition > slideMax) {
-      setSlide(slidePosition - 41.666667)
+  if (slides) {
+    const slideMax = (slides.length - 1) * -41.666667
+
+    next = () => {
+      if (slidePosition > slideMax) {
+        setSlide(slidePosition - 41.666667)
+      }
     }
-  }
 
-  const prev = () => {
-    if (slidePosition < 0) {
-      setSlide(slidePosition + 41.666667)
+    prev = () => {
+      if (slidePosition < 0) {
+        setSlide(slidePosition + 41.666667)
+      }
     }
   }
 
@@ -28,7 +33,9 @@ const QuoteSlider = ({ content, books, slides }) => {
             <h3 className="font-sans text-3xl font-semibold">
               {book.frontmatter.title}
             </h3>
-            <p className="font-bold text-metallic-blue">{book.frontmatter.author}</p>
+            <p className="font-bold text-metallic-blue">
+              {book.frontmatter.author}
+            </p>
             <div className="mb-6 sm:flex">
               <img
                 src={book.frontmatter.image.file}
@@ -45,60 +52,64 @@ const QuoteSlider = ({ content, books, slides }) => {
           </div>
         ))}
 
-      <div className="relative mb-4 overflow-hidden">
-        <div
-          className="-mt-3 -mb-3 -ml-3 transition-transform md:flex"
-          style={{ transform: `translateX(${slidePosition}%)` }}
-        >
-          {slides.map(slide => (
-            <div className="flex-shrink-0 p-3 md:w-5/12">
-              <Slide
-                image={slide.image}
-                alt={slide.image_alt}
-                heading={slide.title}
-                text={slide.text}
-              />
-            </div>
-          ))}
+      {slides && (
+        <div className="relative mb-4 overflow-hidden">
+          <div
+            className="-mt-3 -mb-3 -ml-3 transition-transform md:flex"
+            style={{ transform: `translateX(${slidePosition}%)` }}
+          >
+            {slides.map(slide => (
+              <div className="flex-shrink-0 p-3 md:w-5/12">
+                <Slide
+                  image={slide.image}
+                  alt={slide.image_alt}
+                  heading={slide.title}
+                  text={slide.text}
+                />
+              </div>
+            ))}
+          </div>
+
+          {/* Slide blur */}
+          <div className="absolute top-0 right-0 flex-shrink-0 hidden w-2/12 h-90 slide-blur md:block"></div>
         </div>
+      )}
 
-        {/* Slide blur */}
-        <div className="absolute top-0 right-0 flex-shrink-0 hidden w-2/12 h-90 slide-blur md:block"></div>
-      </div>
+      {slides && (
+        <div className="justify-end hidden underline text-mid-blue md:flex">
+          <button onClick={() => prev()} className="flex items-center mr-2">
+            <svg
+              className="w-4 h-4 rotate-90"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              strokeWidth="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            >
+              <path d="M6 9l6 6 6-6" />
+            </svg>
+            <span>Prev</span>
+          </button>
 
-      <div className="justify-end hidden underline text-mid-blue md:flex">
-        <button onClick={() => prev()} className="flex items-center mr-2">
-          <svg
-            className="w-4 h-4 rotate-90"
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 24 24"
-            fill="none"
-            stroke="currentColor"
-            strokeWidth="2"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-          >
-            <path d="M6 9l6 6 6-6" />
-          </svg>
-          <span>Prev</span>
-        </button>
-
-        <button onClick={() => next()} className="flex items-center ml-2">
-          <span>Next</span>
-          <svg
-            className="w-4 h-4 -rotate-90"
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 24 24"
-            fill="none"
-            stroke="currentColor"
-            strokeWidth="2"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-          >
-            <path d="M6 9l6 6 6-6" />
-          </svg>
-        </button>
-      </div>
+          <button onClick={() => next()} className="flex items-center ml-2">
+            <span>Next</span>
+            <svg
+              className="w-4 h-4 -rotate-90"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              strokeWidth="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            >
+              <path d="M6 9l6 6 6-6" />
+            </svg>
+          </button>
+        </div>
+      )}
     </div>
   )
 }
