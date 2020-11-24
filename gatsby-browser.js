323caaac006b786da17c6da5bf6cc6981cfdb8d3
Jonathan McGlone <jmcglone@umich.edu>
2020-11-24 13:27:11 -0500

    Revert "Merge branch 'universal-header' into 'master'"
    This reverts merge request !8

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,26 +1,7 @@
-import React from 'react'
+/**
+ * Implement Gatsby's Browser APIs in this file.
+ *
+ * See: https://www.gatsbyjs.org/docs/browser-apis/
+ */
 
-
-export const wrapPageElement = ({ element }) => {
-    // props provide same data to Layout as Page element will get
-    // including location, data, etc - you don't need to pass it
-    return (
-      <React.Fragment>
-        <div
-          css={{
-            minHeight: '100%',
-            display: 'grid',
-            gridTemplateRows: 'auto auto 1fr',
-            gridTemplateColumns: '100%',
-          }}
-        >
-          <div>
-            <m-universal-header></m-universal-header>
-          </div>
-	  <div>
-            {element}
-	  </div>
-        </div>
-      </React.Fragment>
-    )
-  }
+// You can delete this file if you're not using it
